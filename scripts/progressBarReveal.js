9ee09040aff0dc37d8601ab9ce54d9b8d3e5c5b1
Mathieu Coutant <mathieu.coutant@kosmopolead.com>
2022-07-20 14:20:11 +0200

    Add progress bars and their animation

---
+++ scripts/progressBarReveal.js
@@ -0,0 +1,18 @@
+/* Code from https://alvarotrigo.com/blog/css-animations-scroll/ */
+
+function reveal() {
+  var revealElements = document.querySelectorAll(".PGReveal");
+  for (var i = 0; i < revealElements.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = revealElements[i].getBoundingClientRect().top;
+      if (elementTop < windowHeight) {
+        revealElements[i].classList.add("active");
+      } else {
+        revealElements[i].classList.remove("active");
+      }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal()
