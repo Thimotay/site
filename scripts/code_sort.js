99eb469930220061793788ffd33d8913c1327244
Mathieu Coutant <mathieu.coutant@kosmopolead.com>
2022-07-22 14:56:11 +0200

    Sort - Insertion without animation

--- scripts/code_sort.js
+++ scripts/code_sort.js
@@ -10,12 +10,20 @@ var selector = document.getElementById("sel");
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var fps = 1;
-var sortType = getSelectorValue();
+var sortType;
+
 var arrayLength = 100;
+var numArray;
+var numArrayV2;
+var step;
+
+setSortType();
+
 
 /* =========== Handle the resize of the canvas =========== */
 updateCanvasSize();
 window.addEventListener("resize", updateCanvasSize);
+
 function updateCanvasSize() {
   canvas.setAttribute('width', window.getComputedStyle(canvas, null).getPropertyValue("width"));
   canvas.setAttribute('height', window.getComputedStyle(canvas, null).getPropertyValue("height"));
@@ -24,19 +32,36 @@ function updateCanvasSize() {
 /* =========== Selector =========== */
 
 function setSortType(){
-  sortType = getSelectorValue();
+  if  (sortType != getSelectorValue()){
+    sortType = getSelectorValue();
+    initializeSort();
+  }
 }
 
 function getSelectorValue(){
-  return selector.selectedIndex
+  return selector.selectedIndex;
+}
+
+function initializeSort() {
+  switch (sortType) {
+    case 0:
+      initializeInsertionSort();
+      break;
+    case 1:
+      initializeQuickSort();
+      break;
+    default:
+      break;
+  }
 }
 
 /* =========== Function repeated =========== */
+
+// TODO:TEMPORAIRE JE PENSE
 var intervalId = window.setInterval(function(){
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   switch (sortType) {
     case 0:
-      drawRect();
+      performInsertionSort();
       break;
     case 1:
       drawRedRect();
@@ -46,20 +71,59 @@ var intervalId = window.setInterval(function(){
       break;
   }
 }, 1000/fps);
+  
+
+function drawVisual(oriX, oriY, width, height, nbArray) {
+  for (let i = 0; i < nbArray.length; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = nbArray[i][1];
+    ctx.rect(oriX + width/nbArray.length*i, oriY + height*( 1 - nbArray[i][0]), width / nbArray.length, height*nbArray[i][0]);
+    ctx.fill();
+  }
+}
+
+function getIndexOfFirstValidValue(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][1] != '#000000') {
+      return i;
+    }
+  }
+  
+}
 
 /* =========== Randomizer =========== */
 
 function randomArray(length) {
   var res = [];
   for (let i = 0; i < length; i++) {
-    res.push(Math.random());
+    res.push([Math.random(), '#FFFFFF']);
+  }
+  return res;
+}
+
+function zeroArray(length) {
+  var res = [];
+  for (let i = 0; i < length; i++) {
+    res.push([0, '#FFFFFF']);
   }
   return res;
 }
 
 /* =========== Insertion Sort =========== */
 
-function drawRect(){
+function performInsertionSort() {
+  for (let i = 0; i < numArray.length; i++) {
+    performInsertionStep(i);
+  }
+}
+
+function initializeInsertionSort() {
+  numArray = randomArray(arrayLength);
+  numArrayV2 = zeroArray(arrayLength);
+}
+
+function drawInsertionSort(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.beginPath();
   ctx.lineWidth = "2";
   ctx.strokeStyle = "#FFFFFF";
@@ -69,16 +133,39 @@ function drawRect(){
   ctx.lineWidth = "4";
   ctx.strokeStyle = "#999999";
   ctx.rect(100, 300, 500, 500);
+  ctx.rect(700, 300, 500, 500);
   ctx.stroke();
 
-  ctx.beginPath();
-  var numArray = randomArray(arrayLength);
-  ctx.fillStyle = "#FFFFFF";
-  for (let i = 0; i < numArray.length; i++) {
-    ctx.rect(100 + 500/numArray.length*i, 800 - 500*numArray[i], 500 / numArray.length, 500*numArray[i]);
-  } 
-  ctx.fill();
+  drawVisual(100, 300, 500, 500, numArray);
+  drawVisual(700, 300, 500, 500, numArrayV2);
 }
+
+function performInsertionStep(step){
+  var index = getIndexOfFirstValidValue(numArray);
+  for (let i = 1; i < numArray.length; i++) {
+    if (numArray[i-1][1] != '#FF0000' && numArray[i-1][1] != '#000000') {
+      numArray[i-1][1] = '#FFFFFF';
+    }
+    if (numArray[i][1] != '#000000') {
+      numArray[i][1] = '#00FF00';
+    }
+    if (numArray[i][0] < numArray[index][0] && numArray[i][1] != '#000000') {
+      numArray[index][1] = '#FFFFFF';
+      index = i;
+      numArray[index][1] = '#FF0000';
+    }
+  }
+  numArray[index][1] = '#000000';
+  numArrayV2[step][0] = numArray[index][0];
+  drawInsertionSort();
+}
+
+/* =========== Quick Sort =========== */
+
+function initializeQuickSort() {
+
+}
+
 function drawRedRect(){
   ctx.beginPath();
   ctx.lineWidth = "2";
@@ -87,6 +174,9 @@ function drawRedRect(){
   ctx.rect(100, 300, 500, 500);
   ctx.stroke();
 }
+
+/* =========== Other Sort =========== */
+
 function drawBlueRect(){
   ctx.beginPath();
   ctx.lineWidth = "2";
@@ -95,5 +185,3 @@ function drawBlueRect(){
   ctx.rect(100, 300, 500, 500);
   ctx.stroke();
 }
-
-
